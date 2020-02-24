2974fc717b35e913fc8c228d3ccc636c59887836
Gregory Hawman <g.hawman@gmail.com>
2020-02-24 13:06:19 -0800

    corrected the position of <Router/> into the index.js file where it should be.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 
 const App = () => (
-  <Router>
     <div>
       <Navigation />
     </div>
-  </Router>
 );
 
 export default App;
